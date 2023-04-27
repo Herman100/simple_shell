65c56685521667ec111a7c4f7bb2afd20a6f365a
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-04-27 04:18:17 +0000

    Simple shell project
     Co-authored-by: Herman Kwamebour <hermankwamebour30@gmail.com>

--- path.c
+++ path.c
@@ -24,14 +24,13 @@ char *_which(char *prmCommandName)
 
 	tmpPaths = paths;
 
-	if (prmCommandName[0] == '.' && prmCommandName[1] == '/' && stat(prmCommandName, &st) == 0)
+	if (prmCommandName[0] == '.' && prmCommandName[1] == '/'
+			&& stat(prmCommandName, &st) == 0)
 		return (prmCommandName);
 
 	while (tmpPaths != NULL)
-	{
-		absolutePath = _generateAbsolutePath(tmpPaths, prmCommandName);
+	{	absolutePath = _generateAbsolutePath(tmpPaths, prmCommandName);
 
-		/* Check if absolute path exist */
 		if (stat(absolutePath, &st) == 0)
 		{
 			_freeList(paths);
@@ -42,7 +41,6 @@ char *_which(char *prmCommandName)
 	}
 	_freeList(paths);
 
-	/* Try to find the command */
 	if (stat(prmCommandName, &st) == 0)
 	{
 		return (prmCommandName);
